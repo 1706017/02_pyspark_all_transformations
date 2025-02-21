340d1a4cf978e1c869b7bb995f8f91464cf4aebd
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-21 22:23:41 +0530

    Update all_transformations.py

--- all_transformations.py
+++ all_transformations.py
@@ -6,4 +6,19 @@ df1 = spark.read.format("csv")\
            .option("path","/path/to/input/file")\
            .load()
 
+#displaying the dataframe the we have read
+ df1.display()
+
 ########################################################
+
+#Concept2: Reading JSON file format data
+
+ df_json = spark.read.format("json")\
+                            .option("multiline",False)\
+                            .option("inferSchema",True)\
+                            .option("path","path/to/json/file")\
+                            .load()
+########################################################
+
+
+
