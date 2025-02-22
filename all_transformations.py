b26d0e0ad0ae40be8e588c91464aca840dacb676
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-22 14:11:07 +0530

    all_transformations.py

--- all_transformations.py
+++ all_transformations.py
@@ -53,6 +53,58 @@ df_csv_with_schema.select('Item_Identifier','Item_Weight','Item_Fat_Content').di
 #case2
 df_csv_with_schema.select(col('Item_Identifier'),col('Item_Weight'),col('Item_Fat_Content')).display()
 ######################################################################################################
+Concept5: .alias() Transformation
+
+df_csv_with_schema.select(col('Item_Identifier').alias('Item_Id')).display()
+######################################################################################################
+Concept6: .filter() Transformation
+
+
+#Scenario -01 to fetch only those records where item_fat_content is Regular
+
+df_csv_with_schema.filter(col('Item_Fat_Content')=='Regular').display()
+
+#Scenario-02 To fetch those records only where item type is soft drinks and Item weight is less than 10
+df_csv_with_schema.filter( (col('Item_Type') == 'Soft Drinks') & (col('Item_Weight')<10) ).display()
+
+
+#Scenario3 : To fetch all the records where outlet size is null and outlet tier is in tier1,tier2
+df_csv_with_schema.filter((col('Outlet_Size').isNull()) & (col('Outlet_Location_Type').isin('Tier 1','Tier 2'))).display()
+
+########################################################################################################
+Concept7:  .withColumnRenamed() Transformation
+It is used to change the column name
+
+
+df_csv_with_schema.withColumnRenamed('Item_Weight','Item_WT').display()
+
+#########################################################################################################
+Concept8: Use of .withColumn() Trnsformation
+
+Use1: To add a new column
+Use2 : To update the preexisting column
+Use3 : To create a new column by doing some computation with the help of existing columns
+
+
+#scenario1: 
+df_csv_with_schema.withColumn('flag',lit('new')).display()
+
+
+#Scenario2:
+df_csv_with_schema.withColumn('Multiply',col('Item_MRP')*col('Item_Outlet_Sales')).display()
+
+#Scenario3:
+df_csv_with_schema.withColumn('Item_Fat_Content',regexp_replace(col('Item_Fat_Content'),'Regular','Reg'))\
+                  .withColumn('Item_Fat_Content',regexp_replace(col('Item_Fat_Content'),'Low Fat','LF')).display()
+
+#################################################################################################################################
+
+Concept9: Type cast in pyspark using .cast() Transformation
+
+
+df_csv_with_schema.withColumn('Item_Weight',col('Item_Weight').cast(IntegerType())).printSchema()
+
+####################################################################################################################################
 
 
 
