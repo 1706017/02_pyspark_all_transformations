a6150f2b4c913eb5101480cd5ade2f4a2587920b
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2025-02-22 12:42:58 +0530

    all_transformations.py

--- all_transformations.py
+++ all_transformations.py
@@ -20,5 +20,34 @@ df1 = spark.read.format("csv")\
                             .load()
 ########################################################
 
+#Concept3: To explicitly define schema of dataframe
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+df_csv_schema = StructType([StructField("Item_Identifier",StringType(),True),\
+                           StructField("Item_Weight",StringType(),True),\
+                           StructField("Item_Fat_Content",StringType(),True),\
+                           StructField("Item_Visibility",DoubleType(),True),\
+                           StructField("Item_Type",StringType(),True),\
+                           StructField("Item_MRP",DoubleType(),True),\
+                           StructField("Outlet_Identifier",StringType(),True),\
+                           StructField("Outlet_Establishment_Year",IntegerType(),True),\
+                           StructField("Outlet_Size",StringType(),True),\
+                           StructField("Outlet_Location_Type",StringType(),True),\
+                           StructField("Outlet_Type",StringType(),True),\
+                           StructField("Item_Outlet_Sales",DoubleType(),True)])
+
+#Attaching the schema to the dataframe
+df_csv_with_schema = spark.read.format("csv")\
+                          .option("header",True)\
+                          .schema(df_csv_schema)\
+                          .option("mode","PERMISSIVE")\
+                          .load("/FileStore/tables/BigMart_Sales.csv")
+######################################################################################################
+
+
+
+
+
 
 
